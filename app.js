b2945a4a6f0c9c269bbe5ccc784f85e7dc627653
Mirna Embaby <mirnaembaby@gmail.com>
2026-04-07 12:20:28 +0200

    Setup MongoDB connection

--- app.js
+++ app.js
@@ -6,18 +6,9 @@ const PORT = 3000;
 
 const MONGO_URI = process.env.MONGO_URI || "mongodb://mongo:27017/tasksdb";
 
-// Function to connect to MongoDB with retry
-async function connectWithRetry() {
-  try {
-    await mongoose.connect(MONGO_URI);
-    console.log("Connected to MongoDB");
-  } catch (err) {
-    console.log("MongoDB connection failed, retrying in 2s...", err);
-    setTimeout(connectWithRetry, 2000); // wait 2 seconds and retry
-  }
-}
-
-connectWithRetry();
+mongoose.connect(MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.log(err));
 
 const taskSchema = new mongoose.Schema({
   id: Number,
@@ -26,25 +17,6 @@ const taskSchema = new mongoose.Schema({
 });
 
 const Task = mongoose.model('Task', taskSchema);
-
-// Add a new task automatically for CI/CD testing
-async function addTestTask() {
-  try {
-    await Task.updateOne(
-      { id: 7 },
-      { id: 7, name: 'Tea', status: 'pending' },
-      { upsert: true } // <-- creates it if it doesn’t exist
-    );
-    console.log("Test task 'Tea' ensured in DB");
-  } catch (err) {
-    console.log("Error adding test task:", err);
-  }
-}
-
-// Wait a few seconds after MongoDB connects, then add task
-mongoose.connection.once('open', () => {
-  addTestTask();
-});
 
 app.get('/tasks', async (req, res) => {
   const tasks = await Task.find();
