cbdbff3c492889aa4808ac46bf260149b83eb0eb
Mirna Embaby <mirnaembaby@gmail.com>
2026-04-07 12:14:44 +0200

    Add Tea to task list

--- init.js
+++ init.js
@@ -3,4 +3,5 @@ db = db.getSiblingDB('tasksdb');
 db.tasks.insertMany([
   { id: 1, name: "Study", status: "done" },
   { id: 2, name: "Gym", status: "pending" }
+  { id: 7, name: Tea, status: "pending" }
 ]);
